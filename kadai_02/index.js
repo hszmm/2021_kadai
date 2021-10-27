014c6cd17477a4a035da21de3f9cab5a493e7882
hszmm <users.noreply.gidhub.com>
2021-10-27 07:45:45 +0000

    kadai_02を提出

---
+++ kadai_02/index.js
@@ -0,0 +1,15 @@
+const firstname="shunsuke"
+const familyname="hisazumi"
+console.log(firstname+","+familyname)
+
+const sum=1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+
+const dog =false
+const cat =true
+console.log(dog||cat)//犬または猫を飼っているかどうか
+console.log(dog&&cat)//犬と猫の両方を飼っている
+console.log(!dog&&!cat)//犬も猫も飼っていない
+console.log(!(dog||cat))
+
+//おまけ
